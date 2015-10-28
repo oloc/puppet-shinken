7b7d60f3d5a0350e1e734ce8470981610c950d55
oloc <oloc@aurore>
2015-10-28 12:50:16 +0100

    spec correction.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 describe 'shinken' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('shinken') }
-    it { should contain_class('install') }
-    it { should contain_class('files') }
-    it { should contain_class('service') }
-    it { should contain_class('module') }
+    it { should contain_class('shinken::users') }
+    it { should contain_class('shinken::install') }
+    it { should contain_class('shinken::files') }
+    it { should contain_class('shinken::service') }
   end
 end

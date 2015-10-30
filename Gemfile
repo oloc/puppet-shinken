b70fd48e2a4f669a0f7a47e7c4ce427dcacc92dd
oloc <oloc@aurore>
2015-10-30 15:27:12 +0100

    Spec revision: Gemfile simplification, add RspecPuppetFacts, etc.

--- Gemfile
+++ Gemfile
@@ -1,16 +1,8 @@
 source "https://rubygems.org"
 
-group :development do
-  gem "puppet-blacksmith"
-end
-
-group :rake do
-  puppetversion = ENV.key?('PUPPET_VERSION') ? "#{ENV['PUPPET_VERSION']}" : ['>= 3.0.0','< 4.0']
-  gem "rake"
-  gem "puppet", puppetversion
-  gem "puppet-lint"
-  gem "rspec-puppet", :git => 'https://github.com/rodjek/rspec-puppet.git'
-  gem "puppet-syntax"
-  gem "puppetlabs_spec_helper"
-  gem 'rspec', '< 2.99'
-end
+puppetversion = ENV.key?('PUPPET_VERSION') ? "#{ENV['PUPPET_VERSION']}" : ['>= 3.0.0','< 4.0']
+gem 'puppet', puppetversion
+gem 'puppet-lint'
+gem 'puppetlabs_spec_helper', '>= 0.1.0'
+gem 'puppet-syntax'
+gem 'rspec-puppet-facts', :require => false
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,10 +3,18 @@ describe 'shinken' do
 
   let(:title) { 'shinken' }
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('shinken::users') }
-    it { should contain_class('shinken::install') }
-    it { should contain_class('shinken::files') }
-    it { should contain_class('shinken::service') }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      context 'with defaults for all parameters' do
+        it { should contain_class('shinken::users') }
+        it { should contain_class('shinken::install') }
+        it { should contain_class('shinken::files') }
+        it { should contain_class('shinken::service') }
+      end
+    end
   end
 end

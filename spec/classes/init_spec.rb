89d8aed566e1fa198df6384a3cc589fb13ecb6dd
oloc <oloc@aurore>
2015-10-30 13:24:35 +0100

    Add os

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 describe 'shinken', :type => 'class' do
 
   let(:title) { 'shinken' }
-
+  on_supported_os.each do |os, facts|
   context 'with defaults for all parameters' do
     it { should contain_class('shinken::users') }
     it { should contain_class('shinken::install') }
     it { should contain_class('shinken::files') }
     it { should contain_class('shinken::service') }
+  end
   end
 end
--- spec/classes/users_spec.rb
+++ spec/classes/users_spec.rb
@@ -3,8 +3,10 @@ describe 'shinken::users', :type => 'class' do
 
   let(:title) { 'shinken::users' }
 
+  on_supported_os.each do |os, facts|
   context 'with defaults for all parameters' do
     it { should contain_group('shinken group') }
     it { should contain_user('shinken account') }
+  end
   end
 end

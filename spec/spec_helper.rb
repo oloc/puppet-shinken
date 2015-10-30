c8fdc93a18506701f90369f763379ca623b40916
oloc <oloc@aurore>
2015-10-30 16:07:43 +0100

    puppet-lint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet-facts'
 
+require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+require 'puppet-lint'
+PuppetLint::Plugins.load_spec_helper

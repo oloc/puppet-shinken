3d22a3467c12872e9962cdfd80a94ad917653e37
oloc <oloc@aurore>
2015-10-30 14:59:43 +0100

    Remove users from init.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -10,7 +10,6 @@ describe 'shinken' do
       end
 
       context 'with defaults for all parameters' do
-        it { should contain_class('shinken::users') }
         it { should contain_class('shinken::install') }
         it { should contain_class('shinken::files') }
         it { should contain_class('shinken::service') }

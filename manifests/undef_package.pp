dfb2f2b7a578a17543d2626b052b6e5b4dfa884a
oloc <oloc@aurore>
2015-11-03 11:22:57 +0100

    Minor correction: ensure argument of the undef_package.

--- manifests/undef_package.pp
+++ manifests/undef_package.pp
@@ -1,10 +1,10 @@
 define shinken::undef_package (
-  $package_ensure = $shinken::ensure,
+  $ensure         = $shinken::ensure,
   $package_name   = $title,
 ){
   if !defined(Package[$package_name]) {
     package { $package_name:
-      ensure => $package_ensure,
+      ensure => $ensure,
     }
   }
 }

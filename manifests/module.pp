38e9ca5556b028acde214fbb059a1189d4655de5
oloc <oloc@aurore>
2015-11-04 15:24:55 +0100

    Style correction: quoted boolean value found

--- manifests/module.pp
+++ manifests/module.pp
@@ -2,7 +2,7 @@ define shinken::module (
   $ensure   = $ensure,
   $mod_name = $title,
 ){
-  if ($ensure in ['present', 'true']) {
+  if ($ensure in ['present', true]) {
     exec{"install_${mod_name}":
       command => "/usr/bin/shinken install ${mod_name}",
       unless  => "/usr/bin/shinken inventory | grep ${mod_name} 2>/dev/null",
